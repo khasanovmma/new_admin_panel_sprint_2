2c7c430a9cde8337b180f32ca1ea15f7e23a9be5
khasanovmma <khasanovmma010@gmail.com>
2023-10-02 21:54:15 +0500

    feat: implement api endpoint

---
+++ docker_compose/empty_project/app/movies/api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from movies.api.v1 import views
+
+urlpatterns = [
+    path('<uuid:pk>', views.MovieDetailApi.as_view()),
+    path('', views.MoviesListApi.as_view())
+]
--- docker_compose/empty_project/entrypoint.sh
+++ docker_compose/empty_project/entrypoint.sh
@@ -1,5 +1,9 @@
-
+#!/bin/sh
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 
-python manage.py runserver 0.0.0.0:8000
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.ru', '$SUPER_USER_PASS') if not User.objects.filter(username='admin').exists() else 0" | python manage.py shell
+
+uwsgi --http :8000 --chdir /opt/app --module config.wsgi:application
+
+exec "$@"
