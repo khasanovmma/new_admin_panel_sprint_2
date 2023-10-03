2b1e18ebe5eaa01022e668192d3e2a7b82e16b16
khasanovmma010@gmail.com <khasanovmma010@gmail.com>
2023-10-03 12:08:22 +0500

    refact: fix bugs

--- docker_compose/empty_project/app/movies/movies/apps.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-from django.apps import AppConfig
-
-
-class MoviesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "movies"
-    verbose_name = _("movies")
--- docker_compose/empty_project/entrypoint.sh
+++ docker_compose/empty_project/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
-
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.ru', '$SUPER_USER_PASS') if not User.objects.filter(username='admin').exists() else 0" | python manage.py shell
+python manage.py loaddata data.json
 
 uwsgi --http :8000 --chdir /opt/app --module config.wsgi:application
 
